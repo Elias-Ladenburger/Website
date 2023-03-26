c86066216774973ec6f0a36beff80aeaa3681692
Elias Ladenburger <elias@ladenburger-family.de>
2023-03-26 22:38:53 +0200

    Add meta tags and description for SEO

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -20,15 +20,14 @@
  <nav class="text-primary">
      <div class="">
          <!-- This is used to open the menu on mobile devices -->
- <div class="md:hidden">
-     <button class="text-4xl font-bold duration-300 hover:opacity-70 mx-2"
-         on:click="{()=>showMenuOnMobile()}">
-             {@html buttonSymbol}
-     </button>
- </div>
+        <div class="md:hidden">
+            <button class="text-4xl font-bold duration-300 hover:opacity-70 mx-2"
+                on:click="{()=>showMenuOnMobile()}">
+                    {@html buttonSymbol}
+            </button>
+        </div>
      </div>
      <ul class=" {showOnMobile} list-reset md:flex justify-end flex-1 items-center font-header">
-         
          {#each menu as navElem}
          <li class="mr-5">
              <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}" on:click="{showMenuOnMobile}">{navElem.name}</a>
