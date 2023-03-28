cf4defda17f328b7b050e047d9cbad9dcc5e9187
Elias Ladenburger <elias@ladenburger-family.de>
2023-03-28 16:22:22 +0200

    Add content to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,20 @@
   >
   <Button label="Lean more!" link="about"/>
 </Hero>
+<div class=" bg-primary text-primaryContrast text-center p-5 md:flex flex-none">
+  <p class="text-lg md:mx-24 ">I am a freelancer who walks the line between business and technology.
+    This allows me to be your <b class="bg-emphasis text-primary">general practitioner for tech</b>: Your <b>first point of contact for all questions relating to software development, procurement and management</b>.
+    I have a foot in the world of small companies and large corporates alike. 
+    <br/><br/>
+    <b class="bg-emphasis text-primary">Small and medium enterprises (SMEs)</b> do not always have the capacity for full-time experts in software development, product management or cyber security.
+    Their focus is on building, marketing and selling their core product and rightfully so.
+    They have the same needs for technology as their large-scale counterparts, but often less frequently and on a smaller scale.
+    Much like a layperson who counts on a medical general practitioner, I will refer you to the right technical expert and prepare any documents you may need on the way.
+    <b>Just like GPs do in medicine, most technical tasks can be solved by an experienced tech generalist.</b>
+    <br/><br/>
+    <b class="bg-emphasis text-primary">Corporates</b> typically profit from project managers who can keep an overview of the many technical and organizational details of a project.
+    Somebody who can navigate the needs of different stakeholders and speak to developers and executives alike.
+    I "grew up" in a corporate environment and will integrate seamlessly into your internal process for product management or software project management.
+    I hold various certifications on Scrum, SAFe and general product management and will leverage my experience and outsider perspective to help you relentlessly improve your processes. 
+  </p>
+</div>
