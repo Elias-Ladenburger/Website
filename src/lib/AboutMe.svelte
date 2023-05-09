56688bad14ab50faf2973fcbbe187d17cdd69e04
Elias Ladenburger <elias@ladenburger-family.de>
2023-05-09 17:38:42 +0200

    Fix description text

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -14,19 +14,22 @@
     <div class="self-end">
       <p>
         I am a freelancer who walks the line between business and technology.
-        This allows me to be your general practitioner for tech: The first
-        person to call when you find that you processes are not working, your
-        projects are stuck or your product does not perform as expected. <br
-        /><br />
+        This allows me to be the general practitioner for tech for your small
+        business: The first person to call when you find that you processes are
+        not working, your projects are stuck or your product does not perform as
+        expected. <br /><br />
       </p>
       <p>
         I have lived and worked across Germany, Austria, Spain and China. I
         "grew up" in a corporate OEM, led a cross-functional team in an
-        eCommerce startup and tried my hand in starting my own business. I was in consulting before going to university, while being in university and after leaving university.
+        eCommerce startup and tried my hand in starting my own business. I was
+        in consulting before going to university, while being in university and
+        after leaving university.
         <br />
       </p>
       <p>
-        I am 26 years old. I never did the same thing twice, but have always applied past learnings to present challenges. I want to help you.
+        I never did the same thing twice, but have always applied past learnings
+        to present challenges. I want to help you.
       </p>
     </div>
   </div>
