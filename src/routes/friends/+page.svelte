4b2b5f3aa3e054e05d8b257cdee371ba070366df
Elias Ladenburger <elias@ladenburger-family.de>
2024-02-09 12:30:50 +0100

    friendship

---
+++ src/routes/friends/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Hero from "$lib/Hero.svelte";
+  import Button from "$lib/Button.svelte";
+  let selfDescription = ["product manager", "software developer", "IT generalist"]
+  let heroCTA = {title:"Help me?", link:"contact"}
+</script>
+
+<div class="bg-white">
+  <div
+    class="md:grid md:grid-cols-2 gap-2 text-lg text-white text-left spacing-y-5 md:spacing-0 items-center bg-navy bg-opacity-80"
+  >
+    <Hero
+      heroTitle="Witalina ist super!"
+      heroSubtitle="Sie ist meine <em class='bg-teal'>beste Freundin</em> und daran gibt es nichts zu rütteln."
+    >
+    </Hero>
+    <img
+      src="./img/Witalina + Elias skifahren.jpg"
+      alt="Elias Ladenburger"
+      class="pt-5"
+    />
+  </div>
+</div>
