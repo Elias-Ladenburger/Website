d79ae878900c37950aea9e9fd83b2da065433ec7
Elias Ladenburger <elias@ladenburger-family.de>
2024-02-09 14:45:49 +0100

    Add best friend

--- src/routes/Susi-ist-toll/+page.svelte
+++ src/routes/Susi-ist-toll/+page.svelte
@@ -1,8 +1,5 @@
 <script>
   import Hero from "$lib/Hero.svelte";
-  import Button from "$lib/Button.svelte";
-  let selfDescription = ["product manager", "software developer", "IT generalist"]
-  let heroCTA = {title:"Help me?", link:"contact"}
 </script>
 
 <div class="bg-white">
@@ -10,12 +7,12 @@
     class="md:grid md:grid-cols-2 gap-2 text-lg text-white text-left spacing-y-5 md:spacing-0 items-center bg-navy bg-opacity-80"
   >
     <Hero
-      heroTitle="Witalina ist super!"
+      heroTitle="Susanne ist super!"
       heroSubtitle="Sie ist meine <em class='bg-teal'>beste Freundin</em> und daran gibt es nichts zu rütteln."
     >
     </Hero>
     <img
-      src="./img/Best Friends Wita.jpg"
+      src="./img/Best Friends Susi.jpg"
       alt="Best Friends"
       class=""
     />
