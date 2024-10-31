c6a68a3514db88b1a2d46b63bb542ec1e1b70f98
Elias Ladenburger <elias@ladenburger-family.de>
2024-10-31 06:55:59 +0100

    Add subpage for Abschiedsgeschenk

---
+++ src/routes/abschiedsgeschenk/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+    <meta http-equiv="Refresh" content="0; url='https://docs.google.com/spreadsheets/d/1P_lXZr3oNT6aFm2p6yS8C4emqwCuOX-WAkeVPYuqHLU/edit?gid=3076679#gid=3076679'" />
+</svelte:head>
